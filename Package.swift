e67a8c6896c7211704b4e0b98d1cb16f310f2b6d
Marius Felkner <mflknr@mail.de>
2024-02-15 19:10:23 +0100

    UPDATE dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 // tuist-plugin-outdated
@@ -18,8 +18,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tuist/ProjectAutomation", from: "3.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        .package(url: "https://github.com/tuist/ProjectAutomation", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/johnsundell/files.git", exact: "4.2.0"),
         .package(url: "https://github.com/johnsundell/shellout.git", exact: "2.3.0"),
         .package(url: "https://github.com/scottrhoyt/swiftytexttable.git", exact: "0.9.0" ),
